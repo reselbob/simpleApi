21c23d5d9379a6e6303d6b4ba759a80cc46abbde
Bob Reselman <breselman@h2wellness.com>
2017-09-05 13:31:23 -0700

    Updated data factory, added JMeter testing

--- data/dataFactory.js
+++ data/dataFactory.js
@@ -5,18 +5,20 @@ module.exports.getList = function getList(name) {
     const nm = name !=null ? name.toLowerCase() : null;
     if(!nm) return {message:'No list name defined'};
     if(name){
+        obj = {};
+        obj.name = nm;
+        obj.list = null;
         switch (nm) {
             case 'stooges':
-                obj = {stooges: ['moe', 'larry', 'curly']};
+                obj.list = ['moe', 'larry', 'curly'];
                 break;
             case 'jetsons':
-                obj = {jetsons: ['george', 'jane', 'judy', 'elroy']};
+                obj.list = ['george', 'jane', 'judy', 'elroy'];
                 break;
             case 'flintstones':
-                obj = {flintstones: ['fred', 'wilma', 'pebbles']};
+                obj.list = ['fred', 'wilma', 'pebbles'];
                 break;
             default:
-                obj = {listname: nm, message: 'unknown list name'}
                 break;
         }
     }
