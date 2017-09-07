f4b1988f38abad4d6fe3046bdffd5e8667139b05
Bob Reselman <breselman@h2wellness.com>
2017-09-06 19:03:40 -0700

    added list data and list cvs generation for jmeter integration

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
+
+const listData = require('./data/listData')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require('./routes/routes.js')(app);
 
+function createListName(){
+    //go through the list and write each
+    //name to a csv file
+
+    let ln = 'list,\n';
+    for (const prop in listData) {
+        if(!ln){
+            ln = prop + ',\n';
+        }else{
+            ln = ln + prop + ',\n';
+        }
+    }
+    fs.writeFileSync('./jMeter/list.csv', ln);
+
+}
+createListName();
 const server = app.listen(3000, function () {
     console.log('Listening on port %s...', server.address().port);
 });
